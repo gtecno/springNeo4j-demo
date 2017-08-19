db3f2b1569b74a302359abe9cfc6cf0edb01550c
Gabriel Calvo <gabriel.calvo@zabud.co>
2017-08-18 22:32:10 -0500

    first commit , project structure  with spring-boot and Neo4j

---
+++ src/main/java/com/demo/sprinbootgneo4j/demospringbootneo4j/Demospringbootneo4jApplication.java
@@ -0,0 +1,12 @@
+package com.demo.sprinbootgneo4j.demospringbootneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospringbootneo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demospringbootneo4jApplication.class, args);
+	}
+}
